fb264140bb1aa46e59b1d94252038721f74ffcdf
Siddharth Priya <s2priya@uwaterloo.ca>
2023-09-17 16:38:30 -0400

    doc: add comment about memhavoc

--- seahorn/include/seahorn_util.h
+++ seahorn/include/seahorn_util.h
@@ -8,7 +8,15 @@ extern "C" {
 #endif
 
 #define ND __declspec(noalias)
-
+/*
+** memhaovoc havocs a contiguous number of bytes starting at ptr upto length
+** size. Note that if the bytes represent a pointer then in the LLVM model, this
+** pointer cannot point to any allocation created using malloc.
+**
+** This is because each malloc always gives a fresh location. This implies that
+** one cannot havoc a pointer and then assume it is equal to another pointer
+** created using malloc.
+*/
 extern ND void memhavoc(void *ptr, size_t size);
 extern ND void sea_printf(const char *format, ...);
 
